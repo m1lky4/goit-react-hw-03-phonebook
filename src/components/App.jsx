21d0a33e1e17b7cbb7e5496a33ffd806a6d2ff65
m1lky4 <temanafanin1337@gmail.com>
2023-05-31 11:30:44 +0200

    all done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,23 @@ export class App extends Component {
     contacts: [],
     filter: ''
   };
+componentDidMount() {
+  const savedContacts = localStorage.getItem('contacts');
+  if (savedContacts) {
+    this.setState({ contacts: JSON.parse(savedContacts) });
+  }
+}
+
+componentDidUpdate(prevProps, prevState) {
+  if (prevState.contacts !== this.state.contacts) {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
+}
+
+componentWillUnmount() {
+  localStorage.removeItem('contacts');
+}
+
 
   handleParentStateChange = (newContact) => {
     this.setState((prevState) => ({
@@ -26,10 +43,12 @@ export class App extends Component {
     });
   };
    handleDeleteContact = (contactId) => {
-    const { contacts } = this.state;
-    const updatedContacts = contacts.filter((contact) => contact.id !== contactId);
-    this.setState({ contacts: updatedContacts });
-  };
+  const { contacts } = this.state;
+  const updatedContacts = contacts.filter((contact) => contact.id !== contactId);
+  this.setState({ contacts: updatedContacts }, () => {
+    localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+  });
+};
 
   render() {
     return (
